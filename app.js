14de3c641c71db09c3765ca94b1baea3a22d1b39
Aravind C Ranjan <aravindcranjan54@gmail.com>
2018-06-06 14:26:31 +0530

    New route added

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
+app.get("/hello", (req, res) => {
+  res.send("Kubernetes is awesome");
+});
+
 app.listen(3000, () => {
   console.log("Server listening at port 3000");
 });
