d244c27028dbe0cfff78b39519f7a0011472ad30
Aravind C Ranjan <aravindcranjan54@gmail.com>
2018-07-30 18:00:06 +0530

    new statsd lib used

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require("express");
-const dogstatsd = require("node-dogstatsd").StatsD;
 const app = express();
+var StatsD = require("dogstatsd-node").StatsD,
+  client = new StatsD(`${process.env.DOGSTATSD_HOST_IP}`, 8125);
 app.get("/", (req, res) => {
-  c = new dogstatsd(`${process.env.DOGSTATSD_HOST_IP}`, 8125);
-  x = new dogstatsd("localhost", 8125);
-  console.log(x);
-  console.log(c);
-  c.increment("fucking.asshole");
-  x.increment("fucking.asshole");
+  // c = new dogstatsd(`${process.env.DOGSTATSD_HOST_IP}`, 8125);
+  // x = new dogstatsd("localhost", 8125);
+  // console.log(x);
+  // console.log(c);
+  // c.increment("fucking.asshole");
+  // x.increment("fucking.asshole");
+  client.set(["foo", "bar"], 42).send(function(error, bytes) {
+    //this only gets called once after all messages have been sent
+    console.log(error);
+    if (error) {
+      console.error("Oh noes! There was an error:", error);
+    } else {
+      console.log("Successfully sent", bytes, "bytes");
+    }
+  });
   res.send("hello World");
 });
 
